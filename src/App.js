bcee977e6efffec1962a5a1e9ecd0056428535b9
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2020-11-08 21:07:06 +0200

    feature/Search bar working

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import axios from "axios";
 import Users from "./users/Users";
+import Search from "./users/Search";
 
 export class App extends Component {
   state = {
     users: [],
     loading: false,
   };
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/users`);
-    console.log(res.data);
-    this.setState({ users: res.data, loading: false });
-  }
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  //Search github users
+  searchUsers = async (text) => {
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Search searchUsers={this.searchUsers} />
         <div className="container">
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
--- src/users/Users.js
+++ src/users/Users.js
@@ -1,40 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import Useritem from "./Useritem";
+import Spinner from "../components/layout/Spinner";
+import PropTypes from "prop-types";
 
-export class Users extends Component {
-  state = {
-    users: [
-      {
-        login: "mojombo",
-        id: 1,
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        login: "defunkt",
-        id: 2,
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt",
-      },
-      {
-        login: "pjhyett",
-        id: 3,
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett",
-      },
-    ],
-  };
-  render() {
+const Users = (props) => {
+  if (props.loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map((user) => (
+        {props.users.map((user) => (
           <Useritem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
 
+Users.protoTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3, 1fr)",
