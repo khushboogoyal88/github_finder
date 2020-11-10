9c359cfc64927c5bc0ccca51cfc3ac662d36ffd8
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2020-11-10 12:05:40 +0200

    React router setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import axios from "axios";
 import Users from "./users/Users";
+import User from "./users/User";
 import Search from "./users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 
 export class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   };
@@ -20,6 +24,15 @@ export class App extends Component {
       `https://api.github.com/search/users?q=${text}&client_id=id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({ users: res.data.items, loading: false });
+  };
+
+  //Search single user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ user: res.data, loading: false });
   };
 
   //Clear user from state
@@ -45,20 +58,33 @@ export class App extends Component {
     const { users, loading } = this.state;
 
     return (
-      <div className="App">
-        <Navbar />
+      <Router>
+        <div className="App">
+          <Navbar />
 
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p>About to search Github Users</p>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+};
+
+export default About;
